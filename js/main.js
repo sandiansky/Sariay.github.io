7aad6162913d6fc967b84c92ba30beabc161dc3f
song-ci <2573454906@qq.com>
2018-09-26 02:00:18 +0800

    Site updated: 2018-09-26 02:00:11

--- js/main.js
+++ js/main.js
@@ -140,7 +140,7 @@ jQuery(document).ready(function($) {
 	(function Annie_Init() {
 		Annie_Preloader();
 		Annie_Nav();
-		Annie_Random();
+		//Annie_Random();
 		Annie_ToTop();
 		//Annie_Comment();
 	})();
