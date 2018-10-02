c8c4f0152f79bbb68ce9cb0769ab9920bf753d77
Sariay <1261347403@qq.com>
2018-10-02 15:21:47 +0800

    Site updated: 2018-10-02 15:21:40

--- js/main.js
+++ js/main.js
@@ -145,3 +145,41 @@ jQuery(document).ready(function($) {
 		//Annie_Comment();
 	})();
 });
+
+//无限加载
+jQuery(document).ready(function($) {
+	if($('#pagination a').length == 0) {
+		$('#pagination').hide();
+	};
+	if($('.commentnavi a').length == 0) {
+		$('.commentnavi').hide();
+	}; //若不存在链接则隐藏
+	//点击下一页的链接(即那个a标签)   
+	$('#pagination a,.commentnavi a').click(function() {
+		$this = $(this);
+		$this.addClass('loading').text("正在加载..."); //给a标签加载一个loading的class属性，可以用来添加一些加载效果   
+		var href = $this.attr("href"); //获取下一页的链接地址   
+		if(href != undefined) { //如果地址存在   
+			$.ajax({ //发起ajax请求   
+				url: href, //请求的地址就是下一页的链接   
+				type: "get", //请求类型是get     
+				error: function(request) {
+					//如果发生错误怎么处理   
+				},
+				success: function(data) { //请求成功   
+					$this.removeClass('loading').text("加载更多"); //移除loading属性   
+					var $res = $(data).find("#post-list,.depth-1"); //从数据中挑出文章数据，请根据实际情况更改   
+					$('#archive,.commentlist').append($res.fadeIn(500)); //将数据加载加进posts-loop的标签中。 
+					
+					var newhref = $(data).find("#pagination a,.commentnavi a").attr("href"); //找出新的下一页链接   
+					if(newhref != undefined) {
+						$("#pagination a,.commentnavi a").attr("href", newhref);
+					} else {
+						$("#pagination,.commentnavi").remove(); //如果没有下一页了，隐藏   
+					}
+				}
+			});
+		}
+		return false;
+	});
+});
